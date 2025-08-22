34a7534acbc88d01aed2df74c9f3f6866520d420
jibin john <jibin@aventusinformatics.com>
2025-08-22 22:48:15 +0530

    add env

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
@@ -60,6 +61,7 @@ app.get("/export", (req, res) => {
 });
 
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
